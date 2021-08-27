318fc2904a6e80b9623d4ff884deaa1e88962b37
Posccis <lkzq33@gmail.com>
2021-08-27 12:17:24 -0300

    Read me corrigido

---
+++ Language Processing/README.mk
@@ -0,0 +1,12 @@
+:man_technologist: 
+
+<h1>Language Processing</h1>
+<h3>Case Study 2</h3><p>- Prática de análise de dados feita com o curso Python for Research.</p>
+<p>Graças ao <b>Project Gutenberg</b>(além de ser a biblioteca digital mais antiga do mundo, é um projeto com o objetivo de conservar diversos conteúdos culturais de forma digital que são de dominio publico) foi possivel ter acesso a 102 livros nas linguas Inglês, Francês, Alemão e Português de diversos autores. E com esses livros fizemos nossas práticas de limpeza de dados e visualização de dados.</p>
+
+
+
+<p>Veja mais no Notebook</p>
+
+<img src= "LanguageProcess.jpeg">
+
